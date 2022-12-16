29b2d37df2f32a36862a5e1e8cded598a3a5e5da
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2022-12-16 16:02:07 +0100

    ex

---
+++ Day2/mathematical_operations.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Mathematical Operations
+
+# PEMDASLR - From left to right
+# Parentheses    ()
+# Exponents      **
+# Multiplication *
+# Division       /
+# Addition       +
+# Subtraction    -
+
+print(3 * 3 + 3 / 3 - 3) # 7.0
+
+print(3 * (3 + 3) / 3 - 3) # 3.0
---
+++ Day2/variables.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Variables
+
+#Strings
+print("Hello"[-1])
+
+#Integer
+print(123+345)
+print(737_445_439)
+
+#Float
+print(3.14159)
+
+#Boolean
+True #1
+False #0
