00d14a895cbbc9580365207ee59c037990ea9e08
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2022-12-17 00:19:46 +0100

    Izi

---
+++ Day2/number_manipulation.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Number manipulation
+
+print("Float", 4 / 2) # Float
+print("Integer", 4 // 2) # Integer
+
+
+result = 8 // 2
+result //= 2
+print(result)
+
+
+result = 4 + 4
+result -= 4
+print(result)
