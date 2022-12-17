e51989daec17713ee5c911706e3810a149d8bc2f
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2022-12-18 00:29:54 +0100

    ex

--- Day2/Tip_Calculator.py
+++ Day2/Tip_Calculator.py
@@ -5,3 +5,11 @@ import os
 clear = lambda: os.system("cls")
 clear()
 
+print("Welcome to the tip calculator!")
+total_bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+
+cost_for_person = total_bill / people * (tip / 100 + 1)
+print("Each person should pay: $", round(cost_for_person, 2))
