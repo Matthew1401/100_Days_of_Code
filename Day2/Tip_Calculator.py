f5cdb213df083bbabc1321df9a821cd979dd6822
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2022-12-17 15:44:14 +0100

    Easy

---
+++ Day2/Tip_Calculator.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Day 2 Project: Tip Calculator
+
+import os
+clear = lambda: os.system("cls")
+clear()
+
---
+++ Day2/f-string.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# f-string
+
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"your score is {score}, your height is {height}, you are winning - it's {isWinning}")
