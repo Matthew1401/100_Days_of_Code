bdf64a8d8d43654f368bcc3804e3b65a605320c0
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2022-12-18 16:41:14 +0100

    something

---
+++ Day3/comparison-operators.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Comparision Operators
+x = 1
+
+# Greather than
+if x > 0:
+    x
+
+# Less than
+if x < 0:
+    x
+
+# Greater than or equal to
+if x >= 0:
+    x
+
+# Less than or equal to
+if x <= 0:
+    x
+
+# Equal to
+if x == 0:
+    x
+
+# Not equal to
+if x != 0:
+    x 
---
+++ Day3/test.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# f-string
+
+print(0.1 + 0.1 + 0.1 == 0.3)
