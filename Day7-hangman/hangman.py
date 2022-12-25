5e88608f88996ebc13d2c221d7be32b9b87d08da
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2022-12-25 20:26:50 +0100

    Hangman - the end

--- Day7-hangman/hangman.py
+++ Day7-hangman/hangman.py
@@ -12,9 +12,8 @@ stages = hangman_art.stages
 print(hangman_art.logo)
 
 
-life = 7
-word_list = hangman_words.word_list
-chosen_word = random.choice(word_list)
+life = 7                                                                                  # Add a value of life by 7
+chosen_word = random.choice(hangman_words.word_list)                                      # Randomly choose a word from another file
 word_as_list = ["_"] * len(chosen_word)
 word_as_string = " ".join(word_as_list)
 
@@ -32,6 +31,7 @@ while "_" in word_as_list:
     life -= 1
     print(f"You guessed {guessed_letter}, that's not in the word. You lose a life.")
   else:
+    clear()
     for number in range(0, len(chosen_word)):
       if chosen_word[number] == guessed_letter:
         word_as_list[number] = guessed_letter                                             # Put the guessed letter into a list
