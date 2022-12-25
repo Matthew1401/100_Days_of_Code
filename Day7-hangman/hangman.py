a48a8efa0a3f314f5ebfc63c8a94fd0eded1c35a
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2022-12-25 20:08:05 +0100

    hangman

--- Day7-hangman/hangman.py
+++ Day7-hangman/hangman.py
@@ -5,14 +5,98 @@ import os
 clear = lambda: os.system('cls')
 clear()
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
+print(""" 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                       """)
+
+life = 7
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_as_list = ["_"] * len(chosen_word)
+word_as_string = " ".join(word_as_list)
 
-guessed_letter = input("Guess the letter: ").lower()
+print(chosen_word)
+print(word_as_string)
 
-for letter in chosen_word:
-    if letter == guessed_letter:
-        print("Right")
-    else:
-        print("Wrong")
+while "_" in word_as_list:
+  if life <= 6:
+    print(stages[life])
+    if life == 0:
+      break
+  guessed_letter = input("Guess the letter: ").lower()
+  if not guessed_letter in chosen_word:
+    life -= 1
+  else:
+    for number in range(0, len(chosen_word)):
+      if chosen_word[number] == guessed_letter:
+        word_as_list[number] = guessed_letter
+        word_as_string = " ".join(word_as_list)
+  print(word_as_string)
+
+if not "_" in word_as_list:
+  print("You win!")
+else:
+  print("You lose.")
