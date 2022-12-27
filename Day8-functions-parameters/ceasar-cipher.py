f108381366feff7c5f3f0f13be3c957d05a3ad85
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2022-12-27 13:59:07 +0100

    ceasar-cipher

--- Day8-functions-parameters/ceasar-cipher.py
+++ Day8-functions-parameters/ceasar-cipher.py
@@ -5,43 +5,24 @@ clear = lambda: os.system('cls')
 clear()
 
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encode(text, shift):
-    text_as_list = list(text.strip())
-    for i in range(0, len(text_as_list)):
-        if text_as_list[i] in alphabet:
-            index_number = alphabet.index(text_as_list[i]) + shift
-            if index_number > 25:
-                index_number -= 26
-            text_as_list[i] = alphabet[index_number]
+def ceasar(text, shift, direction):
+    end_text = ""
+    if direction == 'decode':
+        shift *= -1
+    for letter in text:
+        if letter == ' ':
+            end_text += ' '
         else:
-            continue
-    text = "".join(text_as_list)
-    print(text)
+            position = alphabet.index(letter)
+            new_position = position + shift
+            end_text += alphabet[new_position]
+    print(f"Your {direction}d message is: {end_text}")
 
-
-def decode(text, shift):
-    text_as_list = list(text.strip())
-    for i in range(0, len(text_as_list)):
-        if text_as_list[i] in alphabet:
-            index_number = alphabet.index(text_as_list[i]) - shift
-        if text_as_list[i] in alphabet:
-            text_as_list[i] = alphabet[index_number]
-        else:
-            continue
-    text = "".join(text_as_list)
-    print(text)
-
-
-if direction == 'encode':
-    encode(text, shift)
-elif direction == 'decode':
-    decode(text, shift)
-else:
-    print("Type 'encode' or 'decode'.\nNothing else ;)")
+ceasar(text, shift, direction)
