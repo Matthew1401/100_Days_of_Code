4335f7989f420c49e3b38591f471df5eab6ba18a
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2022-12-27 14:36:37 +0100

    ceasar-cipher

--- Day8-functions-parameters/ceasar-cipher.py
+++ Day8-functions-parameters/ceasar-cipher.py
@@ -1,28 +1,36 @@
 # Day 8 Project: Ceasar Cipher
 
+import art
 import os
 clear = lambda: os.system('cls')
 clear()
 
+print(art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 
 def ceasar(text, shift, direction):
     end_text = ""
     if direction == 'decode':
         shift *= -1
-    for letter in text:
-        if letter == ' ':
-            end_text += ' '
-        else:
-            position = alphabet.index(letter)
+    for char in text:
+        if char in alphabet:
+            position = alphabet.index(char)
             new_position = position + shift
             end_text += alphabet[new_position]
-    print(f"Your {direction}d message is: {end_text}")
+        else:
+            end_text += char
+    print(f"Here's the {direction}d result: {end_text}")
 
-ceasar(text, shift, direction)
+
+choose = ''
+while choose != 'no':
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if shift >= 26:
+        shift %= 26
+    ceasar(text, shift, direction)
+    choose = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+print("Goodbay")
