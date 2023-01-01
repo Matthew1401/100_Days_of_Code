2d107ac8efbe9e4ffcf055d74b497923ba3a509a
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-01 20:06:02 +0100

    100 Days of Code

--- Day-10-FunctionsWithOutputs/Calculator.py
+++ Day-10-FunctionsWithOutputs/Calculator.py
@@ -1,1 +1,33 @@
 # Day 10 Project: Calculator
+
+import os
+clear = lambda: os.system('cls')
+clear()
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the above: ")
