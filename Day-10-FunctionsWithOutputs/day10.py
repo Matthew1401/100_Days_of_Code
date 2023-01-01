308fdb5e8959beb83cfbedc159938e1af58dcb5d
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-01 19:54:15 +0100

    100 Days of Code

--- Day-10-FunctionsWithOutputs/day10.py
+++ Day-10-FunctionsWithOutputs/day10.py
@@ -3,6 +3,7 @@
 # Functions with Outputs
 
 def format_name(f_name, l_name):
+    """Take a first and last name and conect them into a one string.""" # This is a Docstring, this is a comment to describe what our function is doing
     name = (f"{f_name} {l_name}").title()
     return name
 
