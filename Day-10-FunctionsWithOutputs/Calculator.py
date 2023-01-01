f1a84fbb37c06fbc23ac1157e5a9de1fb4ca0aec
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-01 20:36:17 +0100

    100 Days of Code

--- Day-10-FunctionsWithOutputs/Calculator.py
+++ Day-10-FunctionsWithOutputs/Calculator.py
@@ -1,8 +1,8 @@
 # Day 10 Project: Calculator
 
+import art
 import os
 clear = lambda: os.system('cls')
-clear()
 
 def add(n1, n2):
     return n1 + n2
@@ -24,23 +24,29 @@ operations = {
 }
 
 
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol)
-should_continue = True
+def calculator():
+    clear()
+    print(art.logo)
+    num1 = int(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-while should_continue:
-    operation_symbol = input("Pick an operation: ")
-    num2 = int(input("What's the next number?: "))
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num2)
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ") == "y":
-        num1 = answer
-    else:
-        should_continue = False
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
 
 # should_continue = True
 # while should_continue:
