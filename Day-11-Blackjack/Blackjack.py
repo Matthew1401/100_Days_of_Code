fe491dc5e6c6d762c7c911ba0b219c9cff996983
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-02 22:23:10 +0100

    100 Days of Code

--- Day-11-Blackjack/Blackjack.py
+++ Day-11-Blackjack/Blackjack.py
@@ -1,45 +1,37 @@
 # Day 11 Project: Blackjack
 
-import art
+from art import logo
 import random
 import os
 clear = lambda: os.system('cls')
 clear()
 
-def eleven_into_one(hand):
-    index = 0
-    if sum(hand) > 21:
-        for number in hand:
-            if number == 11: hand[index] = 1
-            index += 1
+def eleven_into_one(hand=[]):
+    if 11 in hand and sum(hand) > 21:
+        hand.remove(11)
+        hand.append(1)
     return hand
 
 def player_deck(cards):
     """Giving you a two random cards"""
     hand = [random.choice(cards), random.choice(cards)]
-    index = 0
-    if sum(hand) > 21:
-        for number in hand:
-            if number == 11: hand[index] = 1
-            index += 1
+    if 11 in hand and sum(hand) > 21:
+        hand.remove(11)
+        hand.append(1)
     return hand
 
 def computer_deck(cards):
     """Giving you a dealer hand completly"""
     hand = [random.choice(cards), random.choice(cards)]
-    index = 0
-    if sum(hand) > 21:
-        for number in hand:
-            if number == 11: hand[index] = 1
-            index += 1
+    if 11 in hand and sum(hand) > 21:
+        hand.remove(11)
+        hand.append(1)
     while True:
         if sum(hand) < 17:
             hand.append(random.choice(cards))
-            index = 0
-            if sum(hand) > 21:
-                for number in hand:
-                    if number == 11: hand[index] = 1
-                    index += 1
+            if 11 in hand and sum(hand) > 21:
+                hand.remove(11)
+                hand.append(1)
         if sum(hand) >= 17:
             break
     return hand
@@ -57,29 +49,39 @@ should_continue = True
 while should_continue:
     play_next = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
     clear()
-    print(art.logo)
     if play_next == 'n':
         should_continue = False
         break
-    
+    print(logo)
+
     player_hand = player_deck(cards)
-    current_player_score = sum(player_hand)
-    print(f"Your cards: {player_hand}, current score: {current_player_score}")
+    player_score = sum(player_hand)
+    print(f"Your cards: {player_hand}, current score: {player_score}")
     
     computer_hand = computer_deck(cards)
-    current_computer_score = sum(computer_hand)
+    computer_score = sum(computer_hand)
     print(f"Computer's first card: {computer_hand[0]}")
 
     while take_another_card():   
         player_hand.append(random.choice(cards))
         player_hand = eleven_into_one(player_hand)
-        current_player_score = sum(player_hand)
-        print(f"Your cards: {player_hand}, current score: {current_player_score}")
+        player_score = sum(player_hand)
+        print(f"Your cards: {player_hand}, current score: {player_score}")
         print(f"Computer's first card: {computer_hand[0]}")
-        if current_player_score > 21: break
+        if player_score > 21: break
     
     # Checking who is the winner
-    print(f"Your final hand: {player_hand}, final score: {current_player_score}")
-    print(f"Computer's final hand: {computer_hand}, final score: {current_computer_score}")
+    clear()
+    print(f"Your final hand: {player_hand}, final score: {player_score}")
+    print(f"Computer's final hand: {computer_hand}, final score: {computer_score}")
 
-    
+    if player_score > 21:
+        print("You went over. You lose.")
+    elif computer_score > 21:
+        print("The Dealer was shocked out :O")
+    elif player_score == computer_score:
+        print("It's a draw.")
+    elif player_score > computer_score:
+        print("You win! Congratulations!")
+    else:
+        print("You lose.")
