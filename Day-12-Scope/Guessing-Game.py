a44dfd080b02b92998d330f9b626a566e391d997
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-03 13:27:25 +0100

    100 Days of Code

--- Day-12-Scope/Guessing-Game.py
+++ Day-12-Scope/Guessing-Game.py
@@ -1,2 +1,8 @@
 # Day 12 Project: Guessing Game
 
+from art import logo
+
+print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+difficulity = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
