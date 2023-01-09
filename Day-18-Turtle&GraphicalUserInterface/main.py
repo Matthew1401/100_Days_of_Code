5a745dfdbd4f3d5891fc2f4fd17f5291bf07f35b
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-09 12:15:49 +0100

    Turtle

--- Day-18-Turtle&GraphicalUserInterface/main.py
+++ Day-18-Turtle&GraphicalUserInterface/main.py
@@ -5,12 +5,6 @@ tim.shape("turtle")
 tim.color("red")
 
 
-def draw_a_square():
-    for _ in range(4):
-        tim.forward(100)
-        tim.right(90)
-
-
 def write_a_dashed_line():
     for _ in range(15):
         tim.forward(10)
@@ -19,13 +13,70 @@ def write_a_dashed_line():
         tim.down()
 
 
+def draw_a_triangle():
+    tim.pencolor("red")
+    for _ in range(3):
+        tim.forward(100)
+        tim.right(120)
 
 
+def draw_a_square():
+    tim.pencolor("blue")
+    for _ in range(4):
+        tim.forward(100)
+        tim.right(90)
 
 
+def draw_a_pentagon():
+    tim.pencolor("purple")
+    for _ in range(5):
+        tim.forward(100)
+        tim.right(72)
 
 
+def draw_a_hexagon():
+    tim.pencolor("green")
+    for _ in range(6):
+        tim.forward(100)
+        tim.right(60)
 
+
+def draw_a_heptagon():
+    tim.pencolor("brown")
+    for _ in range(7):
+        tim.forward(100)
+        tim.right(51.43)
+
+
+def draw_a_octagon():
+    tim.pencolor("magenta")
+    for _ in range(8):
+        tim.forward(100)
+        tim.right(45)
+
+
+def draw_a_nonagon():
+    tim.pencolor("cyan")
+    for _ in range(9):
+        tim.forward(100)
+        tim.right(40)
+
+
+def draw_a_decagon():
+    tim.pencolor("navy")
+    for _ in range(10):
+        tim.forward(100)
+        tim.right(36)
+
+
+draw_a_triangle()
+draw_a_square()
+draw_a_pentagon()
+draw_a_hexagon()
+draw_a_heptagon()
+draw_a_octagon()
+draw_a_nonagon()
+draw_a_decagon()
 
 
 screen = Screen()
