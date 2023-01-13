30d8b21b9d988970d00145b97d7b367c56d14b26
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-13 21:32:11 +0100

    Snake moving

--- Day-20-BuildTheSnakeGamePart1/SnakeGameP1.py
+++ Day-20-BuildTheSnakeGamePart1/SnakeGameP1.py
@@ -1,20 +1,32 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
-x = 0
-all_snakes = []
-for _ in range(0, 3):
-    snake = Turtle(shape="square")
-    snake.color("white")
-    snake.up()
-    snake.goto(x=x, y=0)
-    x -= 20
-    all_snakes.append(snake)
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.up()
+    new_segment.goto(position)
+    segments.append(new_segment)
 
+screen.update()
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(1)
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(x=new_x, y=new_y)
+
+    segments[0].forward(20)
 
 screen.exitonclick()
