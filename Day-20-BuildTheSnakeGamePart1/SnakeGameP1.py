907fce7ebc51d866e85665a751f8a64b74212bb4
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-13 20:48:02 +0100

    I've just make a body for my snake

--- Day-20-BuildTheSnakeGamePart1/SnakeGameP1.py
+++ Day-20-BuildTheSnakeGamePart1/SnakeGameP1.py
@@ -1,8 +1,20 @@
 from turtle import Turtle, Screen
 
-snake = Turtle(shape="square")
-snake.color("white")
 screen = Screen()
+screen.setup(width=600, height=600)
 screen.bgcolor("black")
+screen.title("Snake Game")
+
+x = 0
+all_snakes = []
+for _ in range(0, 3):
+    snake = Turtle(shape="square")
+    snake.color("white")
+    snake.up()
+    snake.goto(x=x, y=0)
+    x -= 20
+    all_snakes.append(snake)
+
+
 
 screen.exitonclick()
