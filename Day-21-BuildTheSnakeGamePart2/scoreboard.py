66bcc0757220a6480f975f856c4967b51319eb05
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-14 20:58:08 +0100

    Scoreboard debugging and hit the wall

--- Day-21-BuildTheSnakeGamePart2/scoreboard.py
+++ Day-21-BuildTheSnakeGamePart2/scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Courier', 18, 'normal')
 
 
 class Scoreboard(Turtle):
@@ -10,12 +12,16 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.speed("fastest")
         self.score = 0
-        self.goto(x=0, y=245)
+        self.goto(x=0, y=270)
         self.show_scoreboard()
 
     def show_scoreboard(self):
         self.clear()
-        self.write(f"Scoreboard: {self.score}", move=False, align='center', font=('Arial', 15, 'normal'))
+        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.home()
+        self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
 
     def add_a_score(self):
         self.score += 1
