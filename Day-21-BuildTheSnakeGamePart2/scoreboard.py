8fbcb30297ef994bfad5247ad593e34e0016218c
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-21 19:44:32 +0100

    Updates from day 24

--- Day-21-BuildTheSnakeGamePart2/scoreboard.py
+++ Day-21-BuildTheSnakeGamePart2/scoreboard.py
@@ -12,16 +12,22 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.speed("fastest")
         self.score = 0
+        self.high_score = 0
         self.goto(x=0, y=270)
         self.show_scoreboard()
 
     def show_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", move=False, align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", move=False, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.home()
-        self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.show_scoreboard()
+    # def game_over(self):
+    #     self.home()
+    #     self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
 
     def add_a_score(self):
         self.score += 1
