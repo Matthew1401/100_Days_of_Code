950d1e153c0ff37b1305f2761f4a177fa8bcdca8
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-21 20:02:32 +0100

    Files, Directions and Paths

--- Day-24-FilesDirectionsPath/main.py
+++ Day-24-FilesDirectionsPath/main.py
@@ -1,4 +1,16 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
-file.close()
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# Changing text in our file.
+# with open("my_file.txt", mode="w") as file:
+#     file.write("New text.")
+
+# Appending text to our file
+# with open("my_file.txt", mode="a") as file:
+#     file.write("New text.")
+
+# Creating completely new file.
+with open("new_file.txt", mode="w") as file:
+    file.write("New text.")
+
