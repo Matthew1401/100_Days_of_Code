a9d4fb7acf696f1c1694dcc3bc31416aea1723b8
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-21 20:09:12 +0100

    High score in my snake game!

--- Day-21-BuildTheSnakeGamePart2/scoreboard.py
+++ Day-21-BuildTheSnakeGamePart2/scoreboard.py
@@ -7,12 +7,13 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.up()
         self.color("white")
         self.hideturtle()
         self.speed("fastest")
         self.score = 0
-        self.high_score = 0
         self.goto(x=0, y=270)
         self.show_scoreboard()
 
@@ -22,12 +23,11 @@ class Scoreboard(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.score))
             self.high_score = self.score
         self.score = 0
         self.show_scoreboard()
-    # def game_over(self):
-    #     self.home()
-    #     self.write("GAME OVER", move=False, align=ALIGNMENT, font=FONT)
 
     def add_a_score(self):
         self.score += 1
