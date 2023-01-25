8c4df5f58956e76c4b343d860422351ffbc05485
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-25 14:39:45 +0100

    String and path exercise started

--- Day-24-FilesDirectionsPath/main.py
+++ Day-24-FilesDirectionsPath/main.py
@@ -1,4 +1,4 @@
-with open("C:/Users/Mateusz Konieczny/Desktop/my_file.txt") as file:
+with open("../../../my_file.txt") as file:
     contents = file.read()
     print(contents)
 
@@ -13,4 +13,3 @@ with open("C:/Users/Mateusz Konieczny/Desktop/my_file.txt") as file:
 # Creating completely new file.
 # with open("new_file.txt", mode="w") as file:
 #     file.write("New text.")
-
