a8b820a947460c277f191a45f5d703d06b282aa6
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-30 10:18:20 +0100

    csv and pandas library

---
+++ Day-25-CSVData&PandasLibrary/main.py
@@ -0,0 +1,11 @@
+# with open("weather_data.csv") as file:
+#     data = file.readlines()
+# print(data)
+
+import csv
+
+with open("weather_data.csv") as file:
+    data = csv.reader(file)
+    for row in data:
+        print(row)
+
