1554e282c22dd01ee23314e97d69ced4a09c151e
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-30 10:58:48 +0100

    csv and pandas library

--- Day-25-CSVData&PandasLibrary/main.py
+++ Day-25-CSVData&PandasLibrary/main.py
@@ -1,11 +1,21 @@
 # with open("weather_data.csv") as file:
 #     data = file.readlines()
-# print(data)
+#     print(data)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as file:
+#     data = csv.reader(file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
 
-with open("weather_data.csv") as file:
-    data = csv.reader(file)
-    for row in data:
-        print(row)
+import pandas
 
+data = pandas.read_csv("weather_data.csv")
+temp_list = data["temp"].to_list()
+print(temp_list)
+average_temp = round(sum(temp_list) / len(temp_list), 2)
+print(f"Average: {average_temp}")
