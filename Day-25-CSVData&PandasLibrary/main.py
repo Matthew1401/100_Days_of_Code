97aa69d0cb6f645aa31fe7084531b2f010c741e3
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-30 11:23:27 +0100

    csv and pandas library

--- Day-25-CSVData&PandasLibrary/main.py
+++ Day-25-CSVData&PandasLibrary/main.py
@@ -14,8 +14,31 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-temp_list = data["temp"].to_list()
-print(temp_list)
-average_temp = round(sum(temp_list) / len(temp_list), 2)
-print(f"Average: {average_temp}")
+# data = pandas.read_csv("weather_data.csv")
+# temp_list = data["temp"].to_list()
+# print(temp_list)
+# print(data["temp"].mean())
+# print(data["temp"].max())
+#
+# # Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+#
+# # Get Data in Row
+# print(data[data.day == "Monday"])
+# print(data[data.temp == data.temp.max()])
+#
+# monday = (data[data.day == "Monday"])
+# monday_temp_c = monday.temp
+# monday_temp_f = monday_temp_c * 1.8 + 32
+# print(monday_temp_c)
+# print(monday_temp_f)
+
+# Get DataFrame from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65],
+}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
+
