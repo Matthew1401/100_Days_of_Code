b0f767e493e7f625396c127c9c9048e64f1d1e90
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-31 22:33:39 +0100

    List Comprehension

--- Day-26-ListComprehension/main.py
+++ Day-26-ListComprehension/main.py
@@ -1,7 +1,7 @@
 # List Comprehension
 
 # Without comprehension
-numbers = [1, 2, 3]
+# numbers = [1, 2, 3]
 # new_list = []
 # for n in numbers:
 #     add_1 = n + 1
@@ -9,5 +9,20 @@ numbers = [1, 2, 3]
 
 
 # With comprehension
-new_list = [n for n in numbers]
-print(new_list)
+# new_list = [n + 1 for n in numbers]
+# print(new_list)
+
+
+# name = "Matthew"
+# new_list = [letter.lower() for letter in name]
+# print(new_list)
+
+
+# new_list = [n for n in range(2, 9, 2)]
+# print(new_list)
+
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+short_names = [name for name in names if len(name) <= 5]
+long_names = [name.upper() for name in names if len(name) > 5]
+print(long_names)
