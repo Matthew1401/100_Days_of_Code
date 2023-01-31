1eafb4a3b59b15dc2e7c87242ee0c080d85b5df2
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-01-31 20:04:52 +0100

    List Comprehension

---
+++ Day-26-ListComprehension/main.py
@@ -0,0 +1,13 @@
+# List Comprehension
+
+# Without comprehension
+numbers = [1, 2, 3]
+# new_list = []
+# for n in numbers:
+#     add_1 = n + 1
+#     new_list.append(add_1)
+
+
+# With comprehension
+new_list = [n for n in numbers]
+print(new_list)
