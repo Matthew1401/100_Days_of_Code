01874295b318129b3157d1eddeec29a100185578
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-02-01 22:20:41 +0100

    List Comprehension

--- Day-26-ListComprehension/exercises2.py
+++ Day-26-ListComprehension/exercises2.py
@@ -1,10 +1,10 @@
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# 🚨 Do Not Change the code above 👆
+# 🚨 Do Not Change the code above
 
 # Write your 1 line code 👇 below:
 
-squared_numbers = [n**2 for n in numbers]
+result = [n for n in numbers if n % 2 == 0]
 
 # Write your code 👆 above:
 
-print(squared_numbers)
+print(result)
