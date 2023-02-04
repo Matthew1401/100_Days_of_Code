32c5adcbb5a7b9266338a7309dcfe3c2604c6835
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-02-04 12:04:47 +0100

    NATO project end!

--- Day-26-ListAndDictionariesComprehension/NATO-project/main.py
+++ Day-26-ListAndDictionariesComprehension/NATO-project/main.py
@@ -5,7 +5,6 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 
 user_type = input("Enter a word: ").upper()
-user_type_letter = [letter for letter in user_type]
 
-answer = [nato_alphabet[letter] for letter in user_type_letter]
+answer = [nato_alphabet[letter] for letter in user_type]
 print(answer)
