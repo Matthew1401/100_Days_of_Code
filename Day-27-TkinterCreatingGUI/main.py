bf55fc581a202bf6ba3aee187d70c9ee150d8e1a
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-02-08 22:12:48 +0100

    Tkinter debugging

--- Day-27-TkinterCreatingGUI/main.py
+++ Day-27-TkinterCreatingGUI/main.py
@@ -3,10 +3,11 @@ from tkinter import *
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
 # Label
-my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label = Label(text="New Text ", font=("Arial", 24, "bold"))
+my_label.grid(column=0, row=0)
 
 
 # Button
@@ -16,13 +17,16 @@ def button_clicked():
 
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
 
+new_button = Button(text="New Button", command=button_clicked)
+new_button.grid(column=2, row=0)
 
 # Entry
 input = Entry(width=10)
-input.pack()
 print(input.get())
+input.grid(column=3, row=2)
+
 
 
 
