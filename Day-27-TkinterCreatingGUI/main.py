2d80d76c91ada6a8047f954b94265071a1d3fc92
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-02-08 12:35:39 +0100

    Tkinter

--- Day-27-TkinterCreatingGUI/main.py
+++ Day-27-TkinterCreatingGUI/main.py
@@ -1,12 +1,25 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+# Button
+def button_clicked():
+    my_label.config(text="I got clicked")
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+
+
+
 
 
 window.mainloop()
