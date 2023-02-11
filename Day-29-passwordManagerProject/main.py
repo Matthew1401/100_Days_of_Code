279faa543df4a4e426ebb9b9436d7427ed5b416e
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-02-11 22:34:59 +0100

    Password Manager project gui

--- Day-29-passwordManagerProject/main.py
+++ Day-29-passwordManagerProject/main.py
@@ -8,13 +8,41 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
+window.config(pady=20, padx=20)
 
+# Logo
 canvas = Canvas(width=200, height=200)
-my_pass_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=my_pass_img)
-canvas.pack()
+logo_png = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_png)
+canvas.grid(column=1, row=0)
 
+# Texts
+text_website = Label(text="Website:", font=("Courier", 10))
+text_website.grid(column=0, row=1)
 
+text_email = Label(text="Email/Username:", font=("Courier", 10))
+text_email.grid(column=0, row=2)
+
+text_password = Label(text="Password:", font=("Courier", 10))
+text_password.grid(column=0, row=3)
+
+# Inputs
+input_website = Entry(width=35)
+input_website.grid(column=1, row=1, columnspan=2)
+input_website.focus()
+
+input_email = Entry(width=35)
+input_email.grid(column=1, row=2, columnspan=2)
+
+input_password = Entry(width=21)
+input_password.grid(column=1, row=3)
+
+# Buttons
+button_gen_pass = Button(text="Generate Password")
+button_gen_pass.grid(column=2, row=3)
+
+button_add = Button(text="Add", width=36)
+button_add.grid(column=1, row=4, columnspan=2)
 
 
 
