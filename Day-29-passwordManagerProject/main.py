185c90869175fe0f4e5addb28d1583bec7f69a2c
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-02-12 21:16:55 +0100

    Password Manager project

--- Day-29-passwordManagerProject/main.py
+++ Day-29-passwordManagerProject/main.py
@@ -2,13 +2,22 @@ from tkinter import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    data_text = website_entry.get() + " | " + email_entry.get() + " | " + password_entry.get() + "\n"
+    # with open("data.txt", mode="a") as file:
+    #     file.write(data_text)
+    print(data_text)
+    website_entry.delete(0, 'end')
+    password_entry.delete(0, 'end')
+    website_entry.focus()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(pady=20, padx=20)
+window.config(pady=50, padx=50)
 
 # Logo
 canvas = Canvas(width=200, height=200)
@@ -16,33 +25,34 @@ logo_png = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_png)
 canvas.grid(column=1, row=0)
 
-# Texts
-text_website = Label(text="Website:", font=("Courier", 10))
-text_website.grid(column=0, row=1)
+# Labels
+website_label = Label(text="Website:", font=("Courier", 10))
+website_label.grid(column=0, row=1)
 
-text_email = Label(text="Email/Username:", font=("Courier", 10))
-text_email.grid(column=0, row=2)
+email_label = Label(text="Email/Username:", font=("Courier", 10))
+email_label.grid(column=0, row=2)
 
-text_password = Label(text="Password:", font=("Courier", 10))
-text_password.grid(column=0, row=3)
+password_label = Label(text="Password:", font=("Courier", 10))
+password_label.grid(column=0, row=3)
 
-# Inputs
-input_website = Entry(width=51)
-input_website.grid(column=1, row=1, columnspan=2)
-input_website.focus()
+# Entries
+website_entry = Entry(width=51)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
-input_email = Entry(width=51)
-input_email.grid(column=1, row=2, columnspan=2)
+email_entry = Entry(width=51)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "testemail@git.com")
 
-input_password = Entry(width=33)
-input_password.grid(column=1, row=3)
+password_entry = Entry(width=33)
+password_entry.grid(column=1, row=3)
 
 # Buttons
-button_gen_pass = Button(text="Generate Password")
-button_gen_pass.grid(column=2, row=3, sticky="w")
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3, sticky="w")
 
-button_add = Button(text="Add", width=43)
-button_add.grid(column=1, row=4, columnspan=2)
+add_button = Button(text="Add", width=43, command=save)
+add_button.grid(column=1, row=4, columnspan=2)
 
 
 
