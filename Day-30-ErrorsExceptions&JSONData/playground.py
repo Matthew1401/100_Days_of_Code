5b093d4447ceb74d05e4db7956b8903cadb47211
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-02-16 11:54:21 +0100

    try, except, else, finally

--- Day-30-ErrorsExceptions&JSONData/playground.py
+++ Day-30-ErrorsExceptions&JSONData/playground.py
@@ -1,10 +1,17 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-try:
-    phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+
+
+def generate_phonetic():
     word = input("Enter a word: ").upper()
-    output_list = [phonetic_dict[letter] for letter in word]
-    print(output_list)
-except KeyError:
-    print("Sorry, only letters in alphabet please.")
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+        print(output_list)
+    except KeyError:
+        print("Sorry, only letters in alphabet please.")
+        generate_phonetic()
+
+
+generate_phonetic()
