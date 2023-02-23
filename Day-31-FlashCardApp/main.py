7c1edf541b0f5c2ffe59d4674af9bd7260e6de9f
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-02-23 14:54:38 +0100

    Debugging

--- Day-31-FlashCardApp/main.py
+++ Day-31-FlashCardApp/main.py
@@ -3,9 +3,7 @@ import pandas
 import random
 FONT_UP = ("Ariel", 40, "italic")
 FONT_DOWN = ("Ariel", 60, "bold")
-index = None
-timer = None
-is_card_front = False
+current_card = {}
 
 
 # -------------------------------------------------Data reading------------------------------------------------------- #
@@ -20,49 +18,27 @@ finally:
 
 
 # --------------------------------------------------Functions--------------------------------------------------------- #
-def pick_word():
-    global index, is_card_front
-    index = random.randint(0, words_length - 1)
-    if is_card_front:
-        canvas.itemconfig(displaying_word, text=words[index]['English'])
-    else:
-        canvas.itemconfig(displaying_word, text=words[index]['Polish'])
-
-
-def card_front():
-    global index
-    canvas.itemconfig(displaying_word, text=words[index]['English'], fill='black')
-    canvas.itemconfig(title_word, text='English', fill='black')
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(words)
+    canvas.itemconfig(title_word, text="English", fill="black")
+    canvas.itemconfig(displaying_word, text=current_card["English"], fill="black")
     canvas.itemconfig(card, image=card_front_png)
+    flip_timer = window.after(3000, func=flip_card)
 
 
-def card_back():
-    global index
-    canvas.itemconfig(displaying_word, text=words[index]['Polish'], fill='white')
-    canvas.itemconfig(title_word, text='Polski', fill='white')
+def flip_card():
+    canvas.itemconfig(title_word, text="Polski", fill="white")
+    canvas.itemconfig(displaying_word, text=current_card["Polish"], fill="white")
     canvas.itemconfig(card, image=card_back_png)
 
 
-def application_on():
-    global index, timer, is_card_front
-    if is_card_front:
-        card_back()
-        is_card_front = False
-    else:
-        card_front()
-        is_card_front = True
-    timer = window.after(3000, application_on)
-
-
 def save_word():
-    global index
-    words.remove(words[index])
-    save = {
-        'English': [i['English'] for i in words],
-        'Polish': [i['Polish'] for i in words]
-    }
-    df = pandas.DataFrame(save)
-    df.to_csv("words_to_learn.csv", index=False)
+    words.remove(current_card)
+    data = pandas.DataFrame(words)
+    data.to_csv("words_to_learn.csv", index=False)
+    next_card()
 
 
 # -----------------------------------------------------UI------------------------------------------------------------- #
@@ -70,6 +46,7 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg='#B1DCC5')
 
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526, bg='#B1DCC5', highlightthickness=0)
 card_front_png = PhotoImage(file="images/card_front.png")
@@ -77,7 +54,6 @@ card_back_png = PhotoImage(file="images/card_back.png")
 card = canvas.create_image(400, 263, image=card_front_png)
 title_word = canvas.create_text(400, 150, text="English", font=FONT_UP)
 displaying_word = canvas.create_text(400, 263, text="", font=FONT_DOWN)
-pick_word()
 canvas.grid(column=0, row=0, columnspan=2)
 
 right_image = PhotoImage(file="images/right.png")
@@ -85,9 +61,9 @@ right_button = Button(image=right_image, highlightthickness=0, bg='#B1DCC5', com
 right_button.grid(column=1, row=1)
 
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0, bg='#B1DCC5', command=pick_word)
+wrong_button = Button(image=wrong_image, highlightthickness=0, bg='#B1DCC5', command=next_card)
 wrong_button.grid(column=0, row=1)
 
 
-application_on()
+next_card()
 window.mainloop()
