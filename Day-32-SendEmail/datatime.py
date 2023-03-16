5ed2f2b6cca3cb9eec2458a71cfab4bf0df8992d
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-03-16 13:58:26 +0100

    datetime

---
+++ Day-32-SendEmail/datatime.py
@@ -0,0 +1,10 @@
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=2004, month=1, day=14)
+print(date_of_birth)
