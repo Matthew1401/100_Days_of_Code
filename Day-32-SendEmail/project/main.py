cb29b46fe0bded8d221bcb1841d44b01479689e0
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-03-27 06:34:18 +0200

    Day's project

--- Day-32-SendEmail/project/main.py
+++ Day-32-SendEmail/project/main.py
@@ -1,13 +1,25 @@
-##################### Extra Hard Starting Project ######################
+import pandas as pd
+import random
+import datetime as dt
 
-# 1. Update the birthdays.csv
+
+now = dt.datetime.now()
+month = now.month
+day = now.day
+
+birthday = pd.read_csv("birthdays.csv")
+birthday_dict = birthday.to_dict()
+print(birthday_dict)
 
 # 2. Check if today matches a birthday in the birthdays.csv
+number = 0
+for birthday_month in birthday_dict["month"]:
+    if birthday_dict["month"][birthday_month] == month:
+        if birthday_dict["day"][number] == day:
+            print("yolo")
+    number += 1
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
+# 3. If step 2 is true, pick a random letter from letter templates
+# and replace the [NAME] with the person's actual name from birthdays.csv
 
 
