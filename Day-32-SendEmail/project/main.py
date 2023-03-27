28586988451fa6d2c285510df6f7b60618954797
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-03-27 08:33:00 +0200

    Day's project

--- Day-32-SendEmail/project/main.py
+++ Day-32-SendEmail/project/main.py
@@ -1,31 +1,27 @@
 import pandas as pd
 import random
-import datetime as dt
+from datetime import datetime
 
 
-now = dt.datetime.now()
+now = datetime.now()
 month = now.month
 day = now.day
 
-birthday = pd.read_csv("birthdays.csv")
-birthday_dict = birthday.to_dict()
+data = pd.read_csv("birthdays.csv")
+birthday_dict = data.to_dict()
+print(birthday_dict)
 
 # 2. Check if today matches a birthday in the birthdays.csv
 number = 0
 for birthday_month in birthday_dict["month"]:
     if birthday_dict["month"][birthday_month] == month:
         if birthday_dict["day"][number] == day:
-
-            random_letter = random.randint(1, 3)
-            with open(f"letter_templates/letter_{random_letter}.txt", mode="r+") as letter:
-                letter_str = letter.read()
-                letter_str = letter_str.replace("[NAME]", birthday_dict['name'][number])
-                print(letter_str)
+            file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+            with open(file_path, mode="r+") as letter:
+                content = letter.read()
+                content = content.replace("[NAME]", birthday_dict['name'][number])
+                print(content)
 
     number += 1
-
-# 3. If step 2 is true, pick a random letter from letter templates
-
-# and replace the [NAME] with the person's actual name from birthdays.csv
 
 
