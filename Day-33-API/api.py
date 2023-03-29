b8b49bdea84b1ec62697d7c8bf16b997d7e8d906
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-03-29 17:04:46 +0200

    iss satelit check

--- Day-33-API/api.py
+++ Day-33-API/api.py
@@ -1,13 +1,39 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
+MY_LAT = 52.237049
+MY_LONG = 21.017532
 
-data = response.json()
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+response_iss = requests.get(url="http://api.open-notify.org/iss-now.json")
+response_iss.raise_for_status()
+
+data_iss = response_iss.json()
+
+longitude = float(data_iss["iss_position"]["longitude"])
+latitude = float(data_iss["iss_position"]["latitude"])
 
 iss_position = (longitude, latitude)
 
 print(iss_position)
+
+
+parameter = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameter)
+response.raise_for_status()
+
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+print(sunrise)
+print(sunset)
+
+
+time_now = datetime.now()
+print(time_now.hour)
