793625823dd954bf903e1142698e91512b3ea51c
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-03-29 14:19:36 +0200

    Kanye west APP

--- Day-33-API/main.py
+++ Day-33-API/main.py
@@ -1,13 +1,26 @@
+from tkinter import *
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
 
-data = response.json()
+def get_quote():
+    response = requests.get(url="https://api.kanye.rest")
+    data = response.json()
+    canvas.itemconfig(quote_text, text=data["quote"])
 
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
 
-iss_position = (longitude, latitude)
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
 
-print(iss_position)
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+
+window.mainloop()
