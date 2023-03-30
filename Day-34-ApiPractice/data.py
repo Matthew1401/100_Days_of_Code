76c67f88854f1d06287f5db758c8802fc09d1ff7
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-03-30 09:50:48 +0200

    API quiz with Trivia

--- Day-34-ApiPractice/data.py
+++ Day-34-ApiPractice/data.py
@@ -1,12 +1,13 @@
 import requests
 
-parameter = {
+parameters = {
     "amount": 10,
     "type": "boolean"
 }
 
-question_data = requests.get(url="https://opentdb.com/api.php", params=parameter)
-question_data.raise_for_status()
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
 
-question_data = question_data.json()
-print(question_data)
+data = response.json()
+
+question_data = data["results"]
