e6eda0fc7ae49c2c9b0e8cbca3f6a497a548c081
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-04-02 14:55:16 +0200

    Pixela

--- Day-37-PostPutDeleteRequests/main.py
+++ Day-37-PostPutDeleteRequests/main.py
@@ -1,8 +1,15 @@
 import requests
 import os
+from datetime import datetime
+
 
 USERNAME = os.environ.get("OWM_API_USERNAME_DAY37")
 TOKEN = os.environ.get("OWM_API_TOKEN_DAY37")
+GRAPH_ID = "graph1"
+
+
+print(USERNAME)
+print(TOKEN)
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -19,7 +26,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Praying Graph",
     "unit": "Bible vers and Love",
     "type": "float",
@@ -30,6 +37,38 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+# print(today.strftime("%Y%m%d"))
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many kilometers did you cycle today? "),
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+# PUT
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+
+# DELETE
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
+
 
