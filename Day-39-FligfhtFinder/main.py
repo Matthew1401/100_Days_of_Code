d29cc8aeaaaf1122b3f25eb0342b48a22912749d
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-04-12 15:17:52 +0200

    Flight tracker start

--- Day-39-FligfhtFinder/main.py
+++ Day-39-FligfhtFinder/main.py
@@ -1,1 +1,10 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData,
+# NotificationManager classes to achieve the program requirements.
+
+# TODO 1: Connect with sheety, and get a data from it.
+
+# TODO 2: Connect with Kiwi, and get a data from it.
+
+# TODO 3: Connect with Tequila, and get a data from it.
+
+# TODO 4: Connect with Twilio SMS and make a send sms function
